74546b09b512780df608d242a7e0ad42ea3eba7c
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2022-01-18 16:50:04 +0530

    Refactor the code to use Lamda Function to validate User Entry

---
+++ lamdaexpression/src/com/lamdaexpression/Emailsamples.java
@@ -0,0 +1,25 @@
+package com.lamdaexpression;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface IEmailIDSample {
+	public String validate(String regex, String input);
+}
+
+public class Emailsamples {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter your Email ID :");
+			String input = sc.next();
+			IFName isValiLastName = (pattern, emailID) -> {
+
+				return "The password given is " + Pattern.compile(pattern).matcher(emailID).matches();
+			};
+
+			System.out.println(isValiLastName.validate("^abc(.+)[A-Za-z0-9]{3}+(@+)bl+(.+)[co]*(.[A-Za-z]{2})$", input));
+		}
+	}
+
+}
